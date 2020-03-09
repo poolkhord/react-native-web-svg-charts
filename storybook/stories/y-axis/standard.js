d1bcf3d10803f174f7b645ab02863094a2f08188
Hossein Mohammadi <hosseinm.developer@gmail.com>
2020-03-09 12:12:44 +0330

    chore: migrate y-axis story to hooks

--- storybook/stories/y-axis/standard.js
+++ storybook/stories/y-axis/standard.js
@@ -1,58 +1,50 @@
 import React from "react";
-import { LineChart, YAxis, Grid } from "../../../src";
+import { YAxis, Grid, Chart, useChart, useLayout, useLine } from "../../../src";
+import * as shape from "d3-shape";
 import { View } from "react-native";
 import { Path } from "react-native-svg";
 
-class YAxisExample extends React.PureComponent {
-  render() {
-    const data = [
-      50,
-      10,
-      40,
-      95,
-      -4,
-      -24,
-      85,
-      91,
-      35,
-      53,
-      -53,
-      24,
-      50,
-      -20,
-      -80,
-    ];
+const YAxisExample = () => {
+  const data = [50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80];
 
-    const contentInset = { top: 20, bottom: 20 };
+  const contentInset = { top: 20, bottom: 20 };
 
-    return (
-      <View style={{ height: 200, flexDirection: "row" }}>
-        <YAxis
-          data={data}
-          contentInset={contentInset}
-          svg={{
-            fill: "grey",
-            fontSize: 10,
-          }}
-          numberOfTicks={10}
-          formatLabel={value => `${value}ºC`}
-        />
-        <LineChart
-          style={{ flex: 1, marginLeft: 16 }}
-          data={data}
-          svg={{ stroke: "rgb(134, 65, 244)" }}
-          contentInset={contentInset}
-        >
-          {({ y, ticks, path }) => (
-            <>
-              <Path fill="none" stroke="rgb(134, 65, 244)" d={path} />
-              <Grid {...{ y, ticks }} />
-            </>
-          )}
-        </LineChart>
-      </View>
-    );
-  }
-}
+  const { width, height, onLayout } = useLayout();
+
+  const { x, y, ticks, mappedData } = useChart({
+    width,
+    height,
+    data,
+    contentInset,
+  });
+  const { line } = useLine({
+    mappedData,
+    x,
+    y,
+    curve: shape.curveNatural,
+  });
+
+  return (
+    <View style={{ height: 200, flexDirection: "row" }}>
+      <YAxis
+        data={data}
+        contentInset={contentInset}
+        svg={{
+          fill: "grey",
+          fontSize: 10,
+        }}
+        numberOfTicks={10}
+        formatLabel={value => `${value}ºC`}
+      />
+      <Chart
+        style={{ flex: 1, marginLeft: 16 }}
+        {...{ width, height, onLayout }}
+      >
+        <Path fill="none" stroke="rgb(134, 65, 244)" d={line} />
+        <Grid {...{ y, ticks }} />
+      </Chart>
+    </View>
+  );
+};
 
 export default YAxisExample;
